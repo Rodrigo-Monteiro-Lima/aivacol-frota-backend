5c552a51058e912e2839923b532705a6b63cbd46
Rodrigo Monteiro <rodrigoml2395@gmail.com>
2026-07-02 08:31:09 -0300

    feat: create base entity for models

---
+++ src/src/common/entities/base.entity.ts
@@ -0,0 +1,20 @@
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at!: Date;
+
+  @Column({ name: 'created_by' })
+  created_by!: string;
+}
